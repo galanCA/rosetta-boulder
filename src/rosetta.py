1f57734efad19911779b3f24f1fcda9734f2b858
galanca <cesar.galan@colorado.edu>
2022-08-06 18:16:46 -0600

    Change name of the csv file.

--- src/rosetta.py
+++ src/rosetta.py
@@ -1,5 +1,21 @@
+from multipledispatch import dispatch
+import numpy as np
+import pandas as pd
+import sys
+sys.path.insert(0,'./data')
 
+class Rosetta:
+    #@dispatch(None)
+    def __init__(self) -> None:
+        self.__extract_data('./data/cf-ff-data.csv')
 
+    # Overload the constructor
+    #@dispatch(None, str)
+    #def __init__(self, file) -> None:
+    #    self.__extract_data(file)
 
-class rosetta:
+    def __extract_data(self, file) -> None:
+        self.data = pd.read_csv(file)
+        print(self.data)
+        
     
